ac4edd0a75c9e34b701dea23f9a41cae059508d3
kulisarawiangin <kulisara.w@ku.th>
2024-01-22 19:12:01 +0700

    fix

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-
 from fastai.vision.all import *
 from fastai.vision.widgets import *
 import gradio as gr
+
+# Load learner and get class labels
 learn = load_learner('model.pkl')
-categories = ('Fish', 'Cat')
+categories = learn.dls.vocab
 
 def classify_image(img):
     pred, idx, probs = learn.predict(img)
     return dict(zip(categories, map(float, probs)))
+
 image = gr.Image()
 label = gr.Label()
 examples = ['cat.jpg', 'fish.jpg', 'capybara.jpg']
 
 intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
-intf.launch(inline=False)
+intf.launch(inline=False, share=True)
